b281c3567f21d2b5e6214283dc5acad67ceb839f
snamiki1212 <snamiki1212@gmail.com>
2021-01-22 23:46:28 -0800

    Update title

--- src/lib/useAgeEvents.ts
+++ src/lib/useAgeEvents.ts
@@ -4,28 +4,29 @@ import { uuid } from "./uuid";
 import { EventInput } from "@fullcalendar/react";
 import { RESOURCE_ID__SHARED__AGE } from "../constants/resourceIds";
 
+const getLastYear = () => {
+  const BUFFER_YEAR = 10;
+
+  let d = new Date();
+  d.setFullYear(d.getFullYear() + BUFFER_YEAR);
+  return d.getFullYear();
+};
+
 export const useAgeEvents = () => {
   const [ageEvents, setAgeEvents] = React.useState<EventInput[]>([]);
 
   const calc = React.useCallback((birthday: string | Date) => {
-    const endDate = (() => {
-      let d = new Date();
-      const BUFFER_YEAR = 10;
-      d.setFullYear(d.getFullYear() + BUFFER_YEAR);
-      return d;
-    })();
-
     const birthDate = new Date(birthday);
 
-    // get year numbers
-    const endYear = endDate.getFullYear();
+    // get year num
+    const endYear = getLastYear();
     const startYear = new Date(birthday).getFullYear();
 
     // create years list
     const years = getRangeNumbers(startYear, endYear);
 
     // create EventInput obj
-    const ageEventList = years.map<EventInput>((year) => {
+    const ageEventList = years.map<EventInput>((year, index) => {
       const start = (() => {
         birthDate.setFullYear(year);
         return birthDate.toISOString();
@@ -40,6 +41,7 @@ export const useAgeEvents = () => {
       return {
         id: uuid(),
         resourceId: RESOURCE_ID__SHARED__AGE,
+        title: `age:${index}`,
         start,
         end,
       };
