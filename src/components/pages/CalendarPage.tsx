671dc56a984bd23c0acedd0a5f60b90325ec51a3
snamiki1212 <snamiki1212@gmail.com>
2021-01-23 11:23:19 -0800

    Update profile card

--- src/components/pages/CalendarPage.tsx
+++ src/components/pages/CalendarPage.tsx
@@ -1,30 +1,12 @@
 import React from "react";
-import { DatePicker } from "@material-ui/pickers";
-
-import { useAgeContext } from "../../hooks/useAgeContext";
+import { ProfileCard } from "../organisms/ProfileCard";
 import { FullCalendar } from "../organisms/FullCalendar";
 
 export const CalendarPage: React.VFC = () => {
-  const { birth, age, setBirth } = useAgeContext();
-
-  const handleDateChange = React.useCallback((date: Date | null) => {
-    if (!date) return;
-    console.log(date);
-    setBirth(date.toISOString());
-  }, [setBirth]);
-
   return (
     <div>
-      <div>age:{age}</div>
-      <DatePicker
-        disableFuture
-        openTo="year"
-        format="yyyy-MM-dd"
-        label="Date of birth"
-        views={["year", "month", "date"]}
-        value={birth}
-        onChange={handleDateChange}
-      />
+      <ProfileCard />
+      <hr />
       <FullCalendar />
     </div>
   );
